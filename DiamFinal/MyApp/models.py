d7bcaaca692f7abc051de2c510d59b37b927e093
matheusiscte <mpclq@iscte-iul.pt>
2023-05-13 16:36:30 +0100

    final updates 1?

--- DiamFinal/MyApp/models.py
+++ DiamFinal/MyApp/models.py
@@ -2,52 +2,11 @@ from django.db import models
 from django.utils import timezone
 from datetime import datetime
 from django.contrib.auth.models import User
-#from django.contrib.auth.models import AbstractUser
-
-
-# Create your models here.
-
-# class CustomUser(AbstractUser):
-#     MALE = 'M'
-#     FEMALE = 'F'
-#     GENDER_CHOICES = [
-#         (MALE, 'Male'),
-#         (FEMALE, 'Female'),
-#     ]
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     email = models.EmailField(unique=True)
-#     #password = models.CharField(max_length=128)
-#     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-#     phone_number = models.CharField(max_length=15)
-#     car_brand = models.CharField(max_length=30)
-#     car_model = models.CharField(max_length=30)
-
-    # Adicione quaisquer outros campos ou métodos personalizados necessários
-
-    #def __str__(self):
-     #   return self.username
-
-
-
-
-#def __str__(self):
-  #  return f"{self.partida} - {self.chegada}"
 
 class CustomUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # MALE = 'M'
-    # FEMALE = 'F'
-    # GENDER_CHOICES = [
-    #     (MALE, 'Male'),
-    #     (FEMALE, 'Female'),
-    # ]
     first_name = models.CharField(max_length=30, null=True)
     last_name = models.CharField(max_length=30, null=True)
-    # email = models.EmailField(unique=True)
-    # #password = models.CharField(max_length=128)
-    # gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    #phone_number = models.CharField(max_length=15, null=True)
     car_brand = models.CharField(max_length=30, null=True)
     car_model = models.CharField(max_length=30, null=True)
 
--- DiamFinal/MyApp/views.py
+++ DiamFinal/MyApp/views.py
@@ -1,15 +1,4 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse, HttpResponseRedirect
-# from django.template import loader
-# from .models import Boleia
-# from django.http import JsonResponse
-# from django.urls import reverse
-# from django.contrib.auth import authenticate, login
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.decorators import login_required
-# from .forms import RegistrationForm
-# from django.contrib.auth import logout
-# from django.contrib.auth.models import User
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
@@ -22,83 +11,10 @@ from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 from .forms import BoleiaForm, EditBoleiaForm
 from django.contrib import messages
-
-
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 
 # Create your views here.
-
-# def index(request):
-#     # consulta a base de dados para obter as boleias
-#     boleias = Boleia.objects.all()
-#
-#     # renderiza a página HTML com as boleias e outros dados de contexto
-#     template = loader.get_template('MyApp/index.html')
-#     context = {'boleias': boleias}
-#     return HttpResponse(template.render(context, request))
-#
-# def search(request):
-#     partida = request.GET.get('partida')
-#     chegada = request.GET.get('chegada')
-#
-#     if partida and chegada:
-#         # Filter Boleia objects using exact match on partida and chegada fields
-#         boleias = Boleia.objects.filter(partida__iexact=partida, chegada__iexact=chegada)
-#     else:
-#         if partida:
-#             boleias = Boleia.objects.filter(partida__iexact=partida)
-#         else:
-#             if chegada:
-#                 boleias = Boleia.objects.filter(chegada__iexact=chegada)
-#
-#             else:
-#                 # Fetch all Boleia objects
-#                 boleias = Boleia.objects.all()
-#
-#     return render(request, 'MyApp/index.html', {'boleias': boleias})
-#
-#
-# def reset(request):
-#     boleias = Boleia.objects.all()
-#     return render(request, 'MyApp/index.html', {'boleias': boleias})
-#
-# def about_view(request):
-#     return render(request, 'MyApp/aboutUs.html')
-#
-# def information_view(request):
-#     return render(request, 'MyApp/information.html')
-#
-# def anunciar_view(request):
-#     return render(request, 'MyApp/anunciar.html')
-#
-# def login_view(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return HttpResponseRedirect(reverse('MyApp:index'))
-#         else:
-#             return render(request, 'MyApp/login.html', {'error_message': 'Invalid login credentials'})
-#     else:
-#         return render(request, 'MyApp/login.html')
-#
-#
-# # Register View
-# def register_view(request):
-#     if request.method == 'POST':
-#         form = RegistrationForm(request.POST)
-#         if form.is_valid():
-#             car_brand = form.cleaned_data.get('car_brand')
-#             car_model = form.cleaned_data.get('car_model')
-#             if form.is_valid():
-#                 form.save()
-#     else:
-#         form = RegistrationForm()
-#     return render(request, 'MyApp/register.html', {'form': form})
-
 
 #@login_required(login_url='/MyApp/login')
 def index(request):
@@ -113,7 +29,6 @@ def search(request):
     chegada = request.GET.get('chegada')
 
     if partida and chegada:
-        # Filter Boleia objects using exact match on partida and chegada fields
         boleias = Boleia.objects.filter(partida__iexact=partida, chegada__iexact=chegada)
     else:
         if partida:
@@ -163,11 +78,14 @@ def anunciar_view(request):
 def detalhes(request, boleia_id):
     boleia = get_object_or_404(Boleia, pk=boleia_id)
     if request.method == 'POST':
-        if request.user not in boleia.users.all():  # Check if user is not already in the relationship
+        if request.user not in boleia.users.all():
             boleia.vagas -= 1
             boleia.users.add(request.user)
             boleia.save()
-        return HttpResponseRedirect(reverse('MyApp:detalhes', args=(boleia_id,)))
+            return HttpResponseRedirect(reverse('MyApp:detalhes', args=(boleia_id,)))
+        # else:
+        #     return render(request, 'MyApp/detalhe.html', {'already_in_boleia': "The user is already registered in this ride"})
+
     return render(request, 'MyApp/detalhe.html', {'boleia': boleia})
 
 
@@ -188,18 +106,6 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('MyApp:login'))
 
-# Register View
-# def register_view(request):
-#     if request.method == 'POST':
-#         form = RegistrationForm(request.POST)
-#         if form.is_valid():
-#             car_brand = form.cleaned_data.get('car_brand')
-#             car_model = form.cleaned_data.get('car_model')
-#             if form.is_valid():
-#                 form.save()
-#     else:
-#         form = RegistrationForm()
-#     return render(request, 'MyApp/register.html', {'form': form})
 
 def register_view(request):
     if request.method == 'POST':
@@ -223,30 +129,6 @@ def register_view(request):
     else:
         form = RegistrationForm()
     return render(request, 'MyApp/register.html', {'form': form})
-
-# def register_view(request):
-#     if request.method == 'POST':
-#         form = RegistrationForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get('username')
-#             email = form.cleaned_data.get('email')
-#
-#             # Check if username or email already exists
-#             if User.objects.filter(username=username).exists():
-#                 # Username already exists
-#                 messages.error(request, 'Username already exists.')
-#             elif User.objects.filter(email=email).exists():
-#                 # Email already exists
-#                 messages.error(request, 'Email already exists.')
-#             else:
-#                 form.save()
-#                 return HttpResponseRedirect(reverse('MyApp:index'))
-#     else:
-#         form = RegistrationForm()
-#
-#     # Get all error messages and pass them to the template
-#     error_messages = list(messages.get_messages(request))
-#     return render(request, 'MyApp/register.html', {'form': form, 'error_messages': error_messages})
 
 
 def user_info_view(request):
@@ -277,7 +159,7 @@ def editar_boleia(request, boleia_id):
 def cancelar_vaga(request, boleia_id, user_id):
     boleia = get_object_or_404(Boleia, pk=boleia_id)
     user = get_object_or_404(User, pk=user_id)
-    if user in boleia.users.all():  # Check if user is in the relationship
+    if user in boleia.users.all():
         boleia.users.remove(user)
         boleia.vagas += 1
         boleia.save()
